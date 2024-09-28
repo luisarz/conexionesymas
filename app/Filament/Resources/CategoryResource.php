033b50f91f91a838a51724533fcd3a6ffa87d40b
luisarz <svcomputec@gmail.com>
2024-09-27 21:05:21 -0600

    Category resource to category

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -4,7 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\CategoryResource\Pages;
 use App\Filament\Resources\CategoryResource\RelationManagers;
-use App\Models\Category;
+use App\Models\category;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
